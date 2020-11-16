4585280ff794694345738af968e9aed3033e64c8
aFan <66076485@qq.com>
2020-11-16 22:06:41 +0800

    test

---
+++ cloud-provider-payment8001/src/main/java/com/fan/springcloud/controller/PaymentController.java
@@ -0,0 +1,39 @@
+package com.fan.springcloud.controller;
+
+import com.fan.springcloud.entities.CommonResult;
+import com.fan.springcloud.entities.Payment;
+import com.fan.springcloud.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class PaymentController {
+
+    @Resource
+    private PaymentService paymentService;
+
+    @PostMapping(value = "/payment/create")
+    public CommonResult create(@RequestBody Payment payment){
+        int result = paymentService.create(payment);
+        log.info("*******插入结果"+result);
+        if(result > 0){
+            return new CommonResult(200,"插入成功",result);
+        }else{
+            return new CommonResult(200,"插入失败",null);
+        }
+    }
+
+    @GetMapping(value = "/payment/get/{id}")
+    public CommonResult getPaymentById(@PathVariable("id") Long id){
+        Payment result = paymentService.getPaymentById(id);
+        log.info("*******插入结果"+result);
+        if(result != null){
+            return new CommonResult(200,"查询成功",result);
+        }else{
+            return new CommonResult(200,"没有对应记录",null);
+        }
+    }
+}
---
+++ cloud-provider-payment8001/src/main/java/com/fan/springcloud/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,28 @@
+package com.fan.springcloud.service.impl;
+
+import com.fan.springcloud.dao.PaymentDao;
+import com.fan.springcloud.entities.Payment;
+import com.fan.springcloud.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+
+    //这两个注解都可以
+    //@Autowired
+    @Resource
+    private PaymentDao paymentDao;
+
+    @Override
+    public int create(Payment payment) {
+        return paymentDao.create(payment);
+    }
+
+    @Override
+    public Payment getPaymentById(Long id) {
+        return paymentDao.getPaymentById(id);
+    }
+}
